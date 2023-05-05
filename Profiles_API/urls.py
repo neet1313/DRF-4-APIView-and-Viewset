49a0beb463ca362c5438559d9dba083d2959a5c5
Prabhneet <prabhneet13@gmail.com>
2023-05-05 11:36:03 +0530

    Registering viewset with routers

--- Profiles_API/urls.py
+++ Profiles_API/urls.py
@@ -1,7 +1,13 @@
+from django.urls import path, include
 from Profiles_API import views
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+# Registering Viewsets with routers in DRF
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewset, basename='hello-viewset')
+
 
 urlpatterns = [
+    # This is how API view is configured
     path('hello-view', views.HelloAPIView.as_view())
 ]
-
