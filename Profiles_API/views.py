c5c768db4477ee00b7dc3b5dbe55dfc7a3d588cc
Prabhneet <prabhneet13@gmail.com>
2023-05-05 08:30:50 +0530

    Adding POST function to APIView

--- Profiles_API/views.py
+++ Profiles_API/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from Profiles_API import serializers
 
 
 class HelloAPIView(APIView):
     """Testing API View"""
+
+    serializer_class = serializers.HelloSerializer
 
     # Get Request
     def get(self, request, format=None):
@@ -18,3 +22,15 @@ class HelloAPIView(APIView):
         # Response should consist of either list or dictionary.
         # So that it can be converted into JSON data.
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name."""
+        serializer = self.serializer_class(data = request.data)
+
+        if serializer.is_valid():
+            # Retrieving validated name field from the serializer.
+            name=serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
