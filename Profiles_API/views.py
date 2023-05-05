fdc9c43e79619ad76671023321e5f8268ea992d0
Prabhneet <prabhneet13@gmail.com>
2023-05-05 09:25:15 +0530

    PUT PATCH DELETE functions in APIView

--- Profiles_API/views.py
+++ Profiles_API/views.py
@@ -1,3 +1,9 @@
+# GET-> fetch data
+# POST-> Post data to the DB
+# PUT-> Update data from DB
+# PATCH-> Partially update data from DB
+# DELETE-> Data data from DB
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -27,16 +33,30 @@ class HelloAPIView(APIView):
     def post(self, request):
         """Create a hello message with our name."""
 
-        #--> 1 Data Deserialized 
-        serializer = self.serializer_class(data = request.data)
+        # --> 1 Data Deserialized
+        serializer = self.serializer_class(data=request.data)
 
-        #--> 2 Form Data Validated
+        # --> 2 Form Data Validated
         if serializer.is_valid():
             # Retrieving validated name field from the serializer.
-            name=serializer.validated_data.get('name')
+            name = serializer.validated_data.get('name')
             message = f'Hello {name}'
-        
-        #--> 3 Converted into complex data type and posted to the DB
+
+        # --> 3 Converted into complex data type and posted to the DB
             return Response({'message': message})
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # Creating a PUT, PATCH, DELETE methods
+
+    def put(self, request, pk=None):
+        """To Update an ENTIRE object in DB"""
+        return Response({'message': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """To PARTIALLY update an object in DB"""
+        return Response({'message': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """To delete an object in DB"""
+        return Response({'message': 'DELETE'})
